b7d1f9f17e5c8ae82f616bbad8d49f3183f8b322
Stephen Tvedt <stvedt@kbsp.com>
2015-11-28 16:49:02 -0500

    express function defaults to html as mime type

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(req, res){
-	res.send('Hello Express');
+	res.send('<h1>Hello Express</h1>');
 });
 
 var server = app.listen(3000, function(){
